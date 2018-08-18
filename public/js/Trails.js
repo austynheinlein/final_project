fbe190b9bedca2af554948493dcf57ddc18cf5ea
your name <you@example.com>
2018-08-18 10:28:08 -0700

    trails.js created

---
+++ public/js/Trails.js
@@ -0,0 +1,148 @@
+class Trails extends React.Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      trailsListIsVisible: true,
+      addTrailIsVisible: false,
+      trailIsVisible: false,
+      editTrailIsVisible: false,
+      trails: [],
+      trail: {}
+    }
+    this.toggleState = this.toggleState.bind(this)
+    this.getTrail = this.getTrail.bind(this)
+    this.handleCreate = this.handleCreate.bind(this)
+    this.handleCreateSubmit = this.handleCreateSubmit.bind(this)
+    this.deleteTrail = this.deleteTrail.bind(this)
+    this.handleUpdateSubmit = this.handleUpdateSubmit.bind(this)
+  }
+
+  handleUpdateSubmit(trail){
+    fetch('/trails/' + trail.id, {
+      body: JSON.stringify(trail),
+      method: 'PUT',
+      headers: {
+        "Accept": 'application/json, text/plain, */*',
+        'Content-Type': 'application/json'
+      }
+    })
+    .then(updatedTrail => {
+      return updatedTrail.json()
+    })
+    .then(jsonedTrail => {
+      this.getTrails()
+      this.toggleState('trailsListIsVisible', 'trailIsVisible')
+    })
+    .catch(error => console.log(error))
+  }
+
+  deleteTrail(trail, index){
+    fetch('trails/' + trail.id,
+    {
+      method: 'DELETE'
+    })
+    .then(data => {
+      this.setState({
+        trails: [
+          ...this.state.trails.slice(0, index),
+          ...this.state.trails.slice(index + 1)
+        ]
+      })
+    })
+  })
+
+  handleCreate(trail){
+    this.setState({
+      trails: [trail, ...this.state.trails]
+    })
+  }
+
+  handleCreateSubmit(trail){
+    fetch('/trails', {
+      body: JSON.stringify(trail),
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json, text/plain, */*',
+        'Content-Type': 'application/json'
+      }
+    })
+    .then(createdTrail => {
+      return createdTrail.json()
+    })
+    .then(jsonedTrail => {
+      this.handleCreate(jsonedTrail)
+      this.toggleState('addTrailIsVisible', 'trailsListIsVisible')
+    })
+    .catch(error => console.log(error))
+  }
+
+  getTrail(trail){
+    this.setState({
+      trail: trail
+    })
+  }
+
+  componentDidMount(){
+    this.getTrails()
+  }
+
+  getTrails(){
+    fetch('/trails')
+    .then(response => response.json())
+    .then(data => {
+      this.setState({
+        trails: data
+      })
+    }).catch(error => console.log(error))
+  }
+
+  toggleState (state1, state2, state3) {
+    this.setState({
+      [state1]: !this.state[state1],
+      [state2]: !this.state[state2],
+      [state3]: !this.state[state3]
+    })
+  }
+
+  render() {
+    return(
+
+      <div className='trails'>
+        <h3 className='local'> Dog Friendly Trails </h3>
+        <div className='addtrailbtn'>
+          <button className='addbtn' onClick={()=>this.toggleState('addTrailIsVisible', 'trailsListIsVisible')}>Add a Trail</button>
+        </div>
+        {this.state.trailsListIsVisible ?
+          <TrailsList
+            toggleState={this.toggleState}
+            trails={this.state.trails}
+            getTrail={this.getTrail}
+            deleteTrail={this.deleteTrail}
+          />
+          : ''
+        }
+        {this.state.addTrailIsVisible ?
+          <TrailForm
+            toggleState={this.toggleState}
+            handleCreate={this.handleCreate}
+            handleSubmit={this.handleCreateSubmit}
+            editTrailIsVisible={this.state.editTrailIsVisible}
+          />
+          : ''
+        }
+
+        {this.state.trailIsVisible ?
+          <Trail
+            toggleState={this.toggleState}
+            trail={this.state.trail}
+            handleSubmit={this.handleUpdateSubmit}
+            editTrailIsVisible={this.state.editTrailIsVisible}
+          />
+          : ''
+        }
+      </div>
+      
+    )
+  }
+
+}
